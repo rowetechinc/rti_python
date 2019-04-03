2c55c4a77be622fdc8a26559064ae1bab922ef50
rowetechinc <rcastelo@rowetechinc.com>
2019-04-03 15:26:11 -0700

    Updated merge_adcp_gps and fixed the bugs to make it run faster

--- Utilities/merge_adcp_gps.py
+++ Utilities/merge_adcp_gps.py
@@ -5,6 +5,8 @@ import logging
 from tqdm import tqdm
 import pynmea2
 import datetime
+import time
+import pandas as pd
 
 from rti_python.Codecs.BinaryCodec import BinaryCodec
 from rti_python.Ensemble.NmeaData import NmeaData
@@ -19,12 +21,15 @@ class MergeAdcpGps:
         # off one of the GPS messages (GGA) in the block
         self.LAST_GPS_ID = '$GPRMC'
 
-        # GPS List.  The list will contain a block
-        # The block will contain 1 or more GPS messages.
-        self.gps_dict = {}
+        # GPS Dataframe List.  The list will contain a a NMEA dataset
+        # and a datetime as the index
+        self.gps_df = pd.DataFrame(columns=['dt', 'nmea'])
 
         # ADCP List.  List of all the ADCP data.
         self.adcp_list = []
+
+        # Write in batches, so buffer up the data to write
+        self.batch_write = bytes()
 
         # Load the GPS data
         self.load_gps_dir(gps_folder_path)
@@ -39,6 +44,10 @@ class MergeAdcpGps:
         :param folder_path: GPS folder path.
         :return:
         """
+
+        if not os.path.exists(folder_path):
+            print("GPS Folder does not exist")
+            return
 
         # Get a list of all the files in the GPS dir
         gps_files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
@@ -70,9 +79,9 @@ class MergeAdcpGps:
                 # When the last GPS id is found, add it to the list
                 if self.LAST_GPS_ID in gps_line:
 
-                    # Create a new entry in the dictionary
-                    if nmea_dataset.datetime and nmea_dataset.datetime not in self.gps_dict.keys():
-                        self.gps_dict[nmea_dataset.datetime] = nmea_dataset
+                    # Create a new entry in the dataframe
+                    if nmea_dataset.datetime:
+                        self.gps_df = self.gps_df.append({'dt': datetime.datetime.combine(datetime.datetime.now().date(), nmea_dataset.datetime), 'nmea': nmea_dataset}, ignore_index=True)
 
                     # Create a new dataset
                     nmea_dataset = NmeaData()
@@ -80,12 +89,21 @@ class MergeAdcpGps:
         # Close the file
         f.close()
 
+        # Move all the datetime to the index
+        self.gps_df['datetime'] = pd.to_datetime(self.gps_df['dt'])
+        self.gps_df = self.gps_df.set_index('datetime')
+        self.gps_df.drop(['dt'], axis=1, inplace=True)
+
     def load_adcp_dir(self, folder_path):
         """
         Load all the ADCP data in the ADCP directory.
         :param folder_path: ADCP folder path.
         :return:
         """
+
+        if not os.path.exists(folder_path):
+            print("ADCP Folder does not exist")
+            return
 
         # Get a list of all the files in the GPS dir
         adcp_files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
@@ -95,54 +113,96 @@ class MergeAdcpGps:
             self.read_adcp_data(folder_path + os.sep + adcp)
 
     def read_adcp_data(self, file_path):
+        """
+        Read in the data from the file.  Find all the ensembles.
+        :param file_path: File path to file.
+        :return:
+        """
+        DELIMITER = b'\x80' * 16
+        datetime_delta = datetime.timedelta(seconds=1)          # Time range for GPS data and Ens data
 
         # Create Output file path
         file_name = os.path.splitext(file_path)[0]              # Get the file name
         mod_file_path = file_name + "_gps.ens"                  # Create a new file path
 
-        DELIMITER = b'\x80' * 16
-        datetime_delta = datetime.timedelta(seconds=1)
+        # Create a buffer
+        buff = bytes()
 
-        print("Loading file: " + file_path)
+        with tqdm(total=os.path.getsize(file_path)) as pbar:
 
-        # Open the file
-        file = open(file_path, 'rb')
+            # Open the output file
+            with open(mod_file_path, "wb") as output_file:
+                # Open the file
+                file = open(file_path, 'rb')
 
-        # Split the data by delimiter
-        ens_raw = file.read().split(DELIMITER)
+                data = file.read(4096)                                  # Read in data
+                pbar.update(len(data))                                  # Update the progressbar
 
-        # Close the file
-        file.close()
+                while data:                                             # Verify data was found
+                    buff += data                                        # Accumulate the buffer
+                    if DELIMITER in buff:                               # Check for the delimiter
+                        chunks = buff.split(DELIMITER)                  # If delimiter found, split to get the remaining buffer data
+                        buff = chunks.pop()                             # Put the remaining data back in the buffer
+                        for chunk in chunks:                            # Take out the ens data
+                            self.process_ens_bin(DELIMITER + chunk,     # Combine the delimiter and ens data
+                                                 datetime_delta,        # Time Range
+                                                 output_file)           # Output file
+                    data = file.read(4096)                              # Read the next batch of data
+                    pbar.update(len(data))                              # Update the progressbar
 
-        with open(mod_file_path, "wb") as output_file:
+                # Close the input files
+                file.close()
 
-            for ens_data in tqdm(ens_raw):
-                # Verify the ENS data is good
-                # This will check that all the data is there and the checksum is good
-                if BinaryCodec.verify_ens_data(DELIMITER + ens_data):
-                    # Decode the ens binary data
-                    ens = BinaryCodec.decode_data_sets(DELIMITER + ens_data)
+                # Write the remaining ensembles to the file
+                output_file.write(self.batch_write)
 
-                    # Add the ensemble to the list
-                    if ens:
-                        #self.adcp_list.append(ens)
-                        # Find the GPS in the dictionary matching the datetime
-                        matching_key = [x for x in self.gps_dict.keys() if x and (ens.EnsembleData.datetime() - datetime_delta).time() <= x <= (ens.EnsembleData.datetime() + datetime_delta).time()]
-
-                        # Get the last matching key
-                        nmea_ds = None
-                        if matching_key:
-                            nmea_ds = self.gps_dict[matching_key[-1]]
-
-                        # Add the NMEA data to the file if it exist
-                        if nmea_ds:
-                            ens.AddNmeaData(nmea_ds)
-
-                        # Write the ensemble to the file
-                        output_file.write(ens.encode())
-
-            # Close the file
+            # Close the output file
             output_file.close()
+
+    def process_ens_bin(self, ens_bin, datetime_delta, output_file):
+        """
+        Process an ensemble.  Given the binary data.  Verify you can decode
+        the data.  If you can decode the data, then look for the GPS data associated
+        with the time in the ensemble.  If you find a match, merge the GPS data into
+        the ADCP data.  Then write the ensemble back to the new file.
+
+        Write the output file in batches to save on writes.
+        :param ens_bin: Binary ensemble data to decode.
+        :param datetime_delta: Time difference to find the GPS data that matches
+        :param output_file: Output file path
+        :return:
+        """
+        # Verify the ENS data is good
+        # This will check that all the data is there and the checksum is good
+        if BinaryCodec.verify_ens_data(ens_bin):
+            # Decode the ens binary data
+            ens = BinaryCodec.decode_data_sets(ens_bin)
+
+            # Add the ensemble to the list
+            if ens:
+                # Find the GPS datetime in the dictionary matching the datetime range
+                min_time = (ens.EnsembleData.datetime() - datetime_delta).time()
+                max_time = (ens.EnsembleData.datetime() + datetime_delta).time()
+                matching_key = self.gps_df[min_time:max_time]
+
+                # Get the last matching key
+                nmea_ds = None
+                if not matching_key.empty:
+                    nmea_ds = matching_key[-1:]['nmea'].item()
+
+                # Add the NMEA data to the file if it exist
+                if nmea_ds:
+                    ens.AddNmeaData(nmea_ds)
+
+                # Accumulate the buffer
+                self.batch_write += ens.encode()
+
+                if len(self.batch_write) > 4096 * 30:
+                    # Write the ensemble to the file
+                    output_file.write(self.batch_write)
+
+                    # Clear the buffer
+                    self.batch_write = bytes()
 
 
 if __name__ == '__main__':
@@ -150,10 +210,10 @@ if __name__ == '__main__':
     #FORMAT = '%(asctime)s - %(levelname)s - %(module)s - (%(threadName)-10s) - %(message)s'
     #logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
-    #gps_folder = "G:\\RTI\\data\\uw\\gps"
-    #adcp_folder = "G:\\RTI\\data\\uw\\ADCP"
-    gps_folder = "C:\\RTI\\Data\\uw\\gps"
-    adcp_folder = "C:\\RTI\\Data\\uw\\ADCP"
+    gps_folder = "G:\\RTI\\data\\uw\\gps_min"
+    adcp_folder = "G:\\RTI\\data\\uw\\ADCP_min"
+    #gps_folder = "C:\\RTI\\Data\\uw\\gps_min"
+    #adcp_folder = "C:\\RTI\\Data\\uw\\ADCP_min"
 
 
     MergeAdcpGps(gps_folder, adcp_folder)
