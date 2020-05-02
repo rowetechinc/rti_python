7dcfe9a6af620ab95bb98ff1d48cb12df91f3788
Rico Castelo <rcastelo@rowetechinc.com>
2020-05-01 19:01:49 -0700

    Working on datalogger

--- Datalogger/DataloggerHardware.py
+++ Datalogger/DataloggerHardware.py
@@ -2,16 +2,32 @@ import rti_python.Comm.adcp_serial_port as adcp_serial
 from typing import List
 
 
+def get_serial_ports() -> List[str]:
+    return adcp_serial.get_serial_ports()
+
+def get_baud_rates() -> List[str]:
+    return adcp_serial.get_baud_rates()
+
+def get_serial_ports_tuple() -> List[tuple]:
+    result = []
+
+    for port in adcp_serial.get_serial_ports():
+        result.append((port, port))
+
+    return result
+
+def get_baud_rates_tuple() -> List[tuple]:
+    result = []
+
+    for baud in adcp_serial.get_baud_rates():
+        result.append((baud, baud))
+
+    return result
+
 class DataLoggerHardware:
 
     def __init__(self):
         self.serial = None
-
-    def get_serial_ports(self) -> List[str]:
-        return adcp_serial.get_serial_ports()
-
-    def get_baud_rates(self) -> List[str]:
-        return adcp_serial.get_baud_rates()
 
     def connect_serial(self,
                        port: str,
